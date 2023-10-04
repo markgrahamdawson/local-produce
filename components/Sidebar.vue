c472990020939e87c233fefb701a9b8402792252
Mark Dawson <markgrahamdawson@gmail.com>
2023-10-04 07:10:14 +0100

    added sidebar layout

---
+++ components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div 
+    class="fixed min-h-screen w-64 bg-slate-500 top-0 transition-all duration-500"
+    :class="sidebarOpen ? 'left-0' : '-left-48'"
+    >
+    <button @click="sidebarOpen = !sidebarOpen" class="w-full bg-blue-500">
+      toggle
+    </button>
+    <h1>LocalProduce</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const sidebarOpen = ref(false)
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-red-500">
+    <h1>
+      Mapbox goes here
+    </h1>
+  </div>
+</template>
